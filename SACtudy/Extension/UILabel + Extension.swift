d33cf6c88c4d972ad63cb81e6dd5171423ea93a5
Liuetenant-K <kts4047@gmail.com>
2022-11-09 15:18:27 +0900

    add customizing label

---
+++ SACtudy/Extension/UILabel + Extension.swift
@@ -0,0 +1,49 @@
+//
+//  UILabel + Extension.swift
+//  SACtudy
+//
+//  Created by 김윤수 on 2022/11/09.
+//
+
+import UIKit
+
+extension UILabel {
+    
+    func applyAppearnce(font: FontSet, color: UIColor) {
+        
+        var attr = AttributedString()
+        attr.applyFontSet(font)
+        attr.foregroundColor = color
+        
+        attributedText = NSAttributedString(attr)
+        
+    }
+    
+    func changeAttributes(string: String, font: UIFont, color: UIColor) {
+        
+        guard let text else { return }
+        
+        let range = NSString(string: text).range(of: string)
+        
+        let mutableAttr = NSMutableAttributedString()
+        
+        if let attr = attributedText {
+            mutableAttr.setAttributedString(attr)
+        } else {
+            mutableAttr.setAttributedString(NSAttributedString(string: text))
+        }
+
+        mutableAttr.setAttributes([.font: font, .foregroundColor: color], range: range)
+        
+        attributedText = mutableAttr
+        
+    }
+    
+    convenience init(text: String, font: FontSet, color: UIColor = Asset.Colors.black.color) {
+        self.init()
+        applyAppearnce(font: font, color: color)
+        self.text = text
+    }
+    
+    
+}
