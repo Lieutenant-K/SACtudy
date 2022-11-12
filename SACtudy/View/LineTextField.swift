0e1e2acab701b0328b0dd993b212e01325f30ffc
Liuetenant-K <kts4047@gmail.com>
2022-11-12 13:04:02 +0900

    add line text field effect

--- SACtudy/View/LineTextField.swift
+++ SACtudy/View/LineTextField.swift
@@ -11,6 +11,8 @@ import RxCocoa
 
 class LineTextField: UITextField {
     
+    let disposeBag = DisposeBag()
+    
     private let line = UIView()
     
     private let inset = UIEdgeInsets(top: 8, left: 14, bottom: 8, right: 14)
@@ -22,6 +24,16 @@ class LineTextField: UITextField {
         attributedPlaceholder = NSAttributedString(text: placeholder, font: font, color: Asset.Colors.gray7.color)
         attributedText = NSAttributedString(text: "", font: font, color: Asset.Colors.black.color)
         textAlignment = .left
+    }
+    
+    func binding() {
+        
+
+        rx.isActive.drive(with: self) { textField, value in
+            textField.line.backgroundColor = value ? Asset.Colors.black.color : Asset.Colors.gray3.color
+        }
+        .disposed(by: disposeBag)
+        
     }
     
     func configureLine() {
@@ -39,11 +51,16 @@ class LineTextField: UITextField {
         super.init(frame: .zero)
         configureTextField(placeholder: placeholder, font: font)
         configureLine()
+        binding()
     }
     
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError()
+    }
+    
+    deinit {
+        print("LineTextField deinit")
     }
 
 }
@@ -65,7 +82,20 @@ extension LineTextField {
 }
 
 extension Reactive where Base: UITextField {
-    public var text: ControlProperty<String?> {
-        value
+    
+    public var isActive: Driver<Bool> {
+        let events = [controlEvent(.editingDidBegin).map{true}, controlEvent(.editingDidEnd).map{false}, controlEvent(.editingDidEndOnExit).map{false}]
+        return Observable.merge(events)
+            .asDriver(onErrorJustReturn: false)
+            
+            
+    }
+    
+    public var beginEditing: Observable<Bool> {
+        return controlEvent(.editingDidBegin).map{true}
+    }
+    
+    public var endEditing: Observable<Bool> {
+        return controlEvent(.editingDidEnd).map{false}
     }
 }
